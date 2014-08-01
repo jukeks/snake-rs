8394ed0d715443da9c7c3b94a2302e9995860f64
Jukka-Pekka Virtanen <jukkapekka.virtanen@gmail.com>
2014-08-01 23:59:38 +0300

    Refactored ifs to a match block.

--- src/ai.rs
+++ src/ai.rs
@@ -7,34 +7,44 @@ use ncurses::*;
 pub fn get_input(world: &World) -> i32 {
 	let ref snake = world.snake;
 	
-	let dx: int = world.food.x as int - snake.head.x as int;
-	let dy: int = world.food.y as int - snake.head.y as int;
+	let dx: int = snake.head.x as int - world.food.x as int;
+	let dy: int = snake.head.y as int - world.food.y as int;
 
-	// match sections are to prevent trying to reverse, which snake can not do
-	if dx < 0 {
-		match snake.current_direction {
-			Right => return KEY_UP,
-			_	  => return KEY_LEFT,
-		}
-	} 
-	if dx > 0 {
-		match snake.current_direction {
-			Left => return KEY_UP,
-			_	 =>	return KEY_RIGHT,
-		}
-	}
-	if dy < 0 {
-		match snake.current_direction {
-			Down => return KEY_LEFT,
-			_	 =>	return KEY_UP,
-		}		
-	}
-	if dy > 0 {
-		match snake.current_direction {
-			Up 	=> return KEY_RIGHT,
-			_	=>	return KEY_DOWN,
-		}
-	}
+	let dx_sign: int = if dx < 0 {
+		-1
+	} else if dx == 0 {
+		0
+	} else {
+		1
+	};
+	let dy_sign: int = if dy < 0 {
+		-1
+	} else if dy == 0 {
+		0
+	} else {
+		1
+	};
 
-	return -1;
+	// the ones with direction specified are to prevent trying
+	// to reverse, which the snake can't do
+	match (dx_sign, dy_sign, snake.current_direction) {
+		// need to go left
+		(1, 	_, 	Right) 	=> KEY_UP,
+		(1, 	_, 	_)		=> KEY_LEFT,
+
+		// need to go right
+		(-1, 	_, 	Left) 	=> KEY_UP,
+		(-1, 	_, 	_)		=> KEY_RIGHT,
+
+		// need to go up
+		(_,		1, 	Down) 	=> KEY_LEFT,
+		(_,		1, 	_)		=> KEY_UP,
+
+		// need to go down
+		(_,		-1,	Up) 	=> KEY_RIGHT,
+		(_, 	-1, _)		=> KEY_DOWN,
+
+		// no selection
+		(_, 	_,	_,)		=> -1
+	}
 }
