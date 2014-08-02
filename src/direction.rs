ec31de525776f616ffca208d59f92d77ab599566
Jukka-Pekka Virtanen <jukkapekka.virtanen@gmail.com>
2014-08-02 14:35:44 +0300

    Added mapping directions to keys.

--- src/direction.rs
+++ src/direction.rs
@@ -1,6 +1,30 @@
+
+use ncurses::*;
+
 pub enum Direction {
 	Up,
 	Down,
 	Left,
 	Right,
+}
+
+impl Direction {
+	pub fn from_key(key: i32) -> Option<Direction> {
+		match key {
+			KEY_UP => Some(Up),
+			KEY_DOWN => Some(Down),
+			KEY_LEFT => Some(Left),
+			KEY_RIGHT => Some(Right),
+			_	=> None
+		}
+	}
+
+	pub fn to_key(direction: Direction) -> i32 {
+		match direction {
+			Up 	=> KEY_UP,
+			Down => KEY_DOWN,
+			Left => KEY_LEFT,
+			Right => KEY_RIGHT,
+		}
+	}
 }
