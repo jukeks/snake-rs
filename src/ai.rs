e71c0cce61ce556bd6b3cd174547e5a2b7daeed8
Jukka-Pekka Virtanen <jukkapekka.virtanen@gmail.com>
2014-08-02 14:39:26 +0300

    Added backtracking search for escape analysis.

--- src/ai.rs
+++ src/ai.rs
@@ -12,30 +12,70 @@ pub fn get_input(world: &World) -> i32 {
 
 	let (dx_sign, dy_sign) = signs(snake, food);
 
-	// the ones with direction specified are to prevent trying
-	// to reverse, which the snake can't do
 	let mut decision = decide_based_on_food_direction(dx_sign, dy_sign, snake.current_direction);
 
-	let mut alternatives = vec!{KEY_UP, KEY_DOWN, KEY_RIGHT, KEY_LEFT};
 	let ref snake = world.snake;
-
-	while !alternatives.is_empty() {
-		let ref mut head = snake.head.clone();
-		apply_decision(head, decision);
-
-		if about_to_collide_self(snake, head) 
-			|| about_to_collide_wall(world, head) {
-			decision = alternatives.pop().unwrap();
-			continue;
-		}
-
-		break;
-	}
+	decision = escape_detection(snake, world, decision, 0);
 
 	decision
 }
 
+fn escape_detection(snake: &Snake, world: &World, mut decision: i32, iteration: uint) -> i32 {
+	if iteration > snake.len / 2 {
+		return decision;
+	}
+
+	let mut alternatives = get_alternatives(snake, decision);
+	while !alternatives.is_empty() {
+		decision = alternatives.pop().unwrap();
+		let ref mut s = snake.clone();
+		s.move(Direction::from_key(decision).unwrap());
+
+		if about_to_collide_self(s) 
+			|| about_to_collide_wall(world, s) {
+			continue;
+		}
+		
+		
+		let escapable =  escape_detection(s, world, decision, iteration + 1);
+		if escapable < 0 {
+			continue;
+		}
+
+		return decision;
+	}
+
+	-1
+}
+
+fn get_alternatives(snake: &Snake, decision: i32) -> Vec<i32> {
+	let mut alternatives = vec!{KEY_UP, KEY_DOWN, KEY_RIGHT, KEY_LEFT};
+	let mut to_be_removed = vec!{decision};
+
+	match snake.current_direction {
+		Up 		=> { alternatives.push(KEY_DOWN); },
+		Down 	=> { alternatives.push(KEY_UP); },
+		Right 	=> { alternatives.push(KEY_LEFT); },
+		Left 	=> { alternatives.push(KEY_RIGHT); },
+	}
+	
+	while !to_be_removed.is_empty() {
+		let key = to_be_removed.pop().unwrap();
+		for i in range(0, alternatives.len()) {
+			if alternatives[i] == key {
+				alternatives.remove(i);
+				break;
+			}
+		}
+	}
+
+	alternatives.push(decision);
+	alternatives
+}
+
 fn decide_based_on_food_direction(dx_sign: int, dy_sign: int, current_direction: Direction) -> i32 {
+	// the ones with direction specified are to prevent trying
+	// to reverse, which the snake can't do
 	match (dx_sign, dy_sign, current_direction) {
 		// need to go left, but going right
 		(1, 	1, 	Right) 	=> KEY_UP,
@@ -47,11 +87,11 @@ fn decide_based_on_food_direction(dx_sign: int, dy_sign: int, current_direction:
 
 		// need to go up, but going down
 		(1,		1, 	Down) 	=> KEY_LEFT,
-		(-1,	_,	Down)	=> KEY_RIGHT,
+		(_,		1,	Down)	=> KEY_RIGHT,
 
 		// need to go down, but going up
 		(1,		-1,	Up) 	=> KEY_LEFT,
-		(-1,	_,	Up)		=> KEY_RIGHT,
+		(_,		-1,	Up)		=> KEY_RIGHT,
 
 		(1, 	_, 	_)		=> KEY_LEFT,
 		(-1, 	_, 	_)		=> KEY_RIGHT,
@@ -85,28 +125,11 @@ fn signs(snake: &Snake, food: &Point) -> (int, int) {
 	(dx_sign, dy_sign)
 }
 
-fn apply_decision(point: &mut Point, decision: i32) {
-	match decision {
-		KEY_UP 		=> point.y -= 1,
-		KEY_DOWN	=> point.y += 1,
-		KEY_RIGHT	=> point.x += 1,
-		KEY_LEFT	=> point.x -= 1,
-		_			=> {}
-	}	
+fn about_to_collide_self(snake: &Snake) -> bool {
+	Point::overlapping(snake.body())
 }
 
-fn about_to_collide_self(snake: &Snake, head: &Point) -> bool {
-	let body = snake.body();
-	for p in body.iter() {
-		if *head == *p {
-			return true;
-		}
-	}
-
-	false
-}
-
-fn about_to_collide_wall(world: &World, head: &Point) -> bool {
-	head.x == 0u - 1 || head.x == world.width ||
-		head.y == 0u - 1 || head.y == world.height
+fn about_to_collide_wall(world: &World, snake: &Snake) -> bool {
+	let ref head = snake.head;
+	head.x > world.width - 1 || head.y > world.height - 1
 }
